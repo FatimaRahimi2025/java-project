0975ff004cf0a7ba1bf2ac3460fd736f84a57f8e
FatimaRahimi2025 <rahimifatima432@gmail.com>
2025-03-11 13:41:26 +0430

    add javascript code

---
+++ script.js
@@ -0,0 +1,7 @@
+const open = document.querySelector('#open');
+const close = document.querySelector('#close');
+const container = document.querySelector('.contianer');
+
+open.addEventListener('click', function(){
+    container.classList.add('show-nav');
+})
